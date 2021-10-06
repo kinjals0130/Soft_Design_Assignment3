411c3b8d26ea72c67ae0ab956e3e4ef6e12f86f2
kinjals0130 <73653330+kinjals0130@users.noreply.github.com>
2021-10-06 09:55:04 -0400

    Update Display.java

--- exercise3/Display.java
+++ exercise3/Display.java
@@ -7,10 +7,6 @@ public class Display implements View{
      * Displays some text on the screen
      * @param text: the text that gets displayed
      */
-    /*public void displayText(String text) {
-        System.out.println("-------------Display-------------");
-        System.out.println(text);
-    }*/
 
     public void displayProduct(String text){
         System.out.println("-------------Display-------------");
