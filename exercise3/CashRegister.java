a351eec2ab1eb64702f939d081bf2d24b2df642a
Danial Shaikh <80362779+Danial-Shaikh@users.noreply.github.com>
2021-10-07 09:57:38 -0400

    Update CashRegister.java

--- exercise3/CashRegister.java
+++ exercise3/CashRegister.java
@@ -1,72 +1,44 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Observable;
 
-public class CashRegister {
+/**
+ * Model
+ * Looks up the price and name of a product based on a UPC
+ */
+public class CashRegister extends Observable {
+    private Display display = new Display();
+    private TicketPrinter ticketPrinter = new TicketPrinter();
+    private Product currentProduct;
+    private ProductDB productDB = new ProductDB();
+    private int UPCCode;
 
-private final ProductDB productDb;
-private long currentUpcCode;
-private final Display display;
-private final TicketPrinter ticketPrinter;
-private final Scanner sc;
-ArrayList<Observer>   ol;
+    /**
+     * Sets the UPC code for current scanned product
+     *
+     * @param UPCCode: barcode value
+     */
+    public void setCurrentProductUPC(int UPCCode) {
+        this.UPCCode = UPCCode;
+        this.currentProduct = this.getCurrentProductInfo();
+        this.addObserver(display);
+        this.addObserver(ticketPrinter);
 
-public CashRegister(Display display, TicketPrinter ticketPrinter) {
-productDb = new ProductDB();
-currentUpcCode = -1;
-this.display = display;
-this.ticketPrinter = ticketPrinter;
-sc = new Scanner(System.in);
-}
+        // Only print on the views if the product is defined
+        if (this.currentProduct != null) {
+            setChanged();
+            notifyObservers(this.currentProduct.toString());
+            // Displays the current scanned item
+//            this.display.displayText(this.currentProduct.toString());
+//            this.ticketPrinter.displayText(this.currentProduct.toString());
+        }
+    }
 
-public void setCurrentProductUPC(long upcCode) {
-currentUpcCode = upcCode;
+    /**
+     * Gets the product information for the latest scanned product
+     *
+     * @return - returns the current Product info
+     */
+    public Product getCurrentProductInfo() {
+        currentProduct = this.productDB.GetProductInfo(this.UPCCode);
+        return currentProduct;
+    }
 }
-
-public long processScanner(){
-Product choice = null;
-int inputChoice = -1;
-ArrayList<Product> db = productDb.getDB();
-
-while(inputChoice == -1){
-System.out.println("Please select a product to scan:");
-int i = 0;
-for (Product product : db) {
-System.out.println(i + "- " + product.getName());
-i++;
-}
-inputChoice = sc.nextInt();
-if(inputChoice >= 0 && inputChoice <= 9){
-choice = db.get(inputChoice);
-}
-else {
-inputChoice = -1;
-System.out.println("Please Enter a Number between 1 and 10...");
-continue;
-}
-}
-
-long upc = choice.getUpcCode();
-
-return upc;
-}
-
-public void getCurrentProductInfo() {
-Product pro = null;
-if (currentUpcCode != -1) {
-pro = productDb.getProductInfo(currentUpcCode);
-}
-if (pro != null) {
-display.displayText(pro.toString());
-ticketPrinter.displayText(pro.toString());
-} else {
-System.out.println("Product with given UPC Code " + currentUpcCode + " could not found in the ProductDB!");
-}
-}
-public void update(double diss) {
-      
-       double discountVal=diss;
-System.out.println("you have added a "+(discountVal*100)+" to your account");
-
-   }
-}
-
