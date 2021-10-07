c80ce8a457e37cc82923d44edfa7f5db536cc5e6
Danial Shaikh <80362779+Danial-Shaikh@users.noreply.github.com>
2021-10-07 09:59:29 -0400

    Update Display.java

--- exercise3/Display.java
+++ exercise3/Display.java
@@ -1,12 +1,23 @@
-public class Display {
+import java.util.Observable;
+import java.util.Observer;
 
-public Display() {
+/**
+ * View
+ * Graphical display of scanned or manually entered item name and price
+ */
+public class Display implements Observer {
+    /**
+     * Displays some text on the screen
+     *
+     * @param text: the text that gets displayed
+     */
+    public void displayText(String text) {
+        System.out.println("-------------Display-------------");
+        System.out.println(text);
+    }
 
-}
-
-public void displayText(String text) {
-System.out.println("Displaying the Product: ");
-System.out.println(text);
-}
-  
+    @Override
+    public void update(Observable o, Object arg) {
+        displayText(String.valueOf(arg));
+    }
 }
