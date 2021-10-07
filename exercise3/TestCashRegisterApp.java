78f7a33bbdb52d3b0f0720b7a1ee1bd127fada5f
Danial Shaikh <80362779+Danial-Shaikh@users.noreply.github.com>
2021-10-07 10:10:10 -0400

    Create TestCashRegisterApp.java

---
+++ exercise3/TestCashRegisterApp.java
@@ -0,0 +1,9 @@
+public class TestCashRegisterApp {
+    public static void main(String[] args) {
+        ScannerClass scanner = new ScannerClass();
+        Keyboard keyboard = new Keyboard();
+
+        scanner.scannedUPCCode(1234);
+        keyboard.setUPCCode(5678);
+    }
+}
